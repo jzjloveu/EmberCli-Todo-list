2623dd383142b956098eda1d515fcb834ab7130b
jzjloveu <jzjloveu@github.com>
2014-08-22 13:04:13 -0700

    second commit

---
+++ app/adapters/application.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+ 
+//export default DS.FixtureAdapter.extend();
+export default DS.LSAdapter.extend({
+  namespace: 'todos-emberjs'
+});
---
+++ app/routes/todos/completed.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+var CompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+export default CompletedRoute;
